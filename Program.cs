be14acacedbd0f9ea3b86e1b545c713f71b713ba
moacir <moacir9401@gmail.com>
2024-07-02 23:28:44 -0300

    Atualizações em Reserva.cs e Program.cs
    Adicionadas verificações e melhorias em Reserva.cs, incluindo a verificação da capacidade da suíte em relação ao número de hóspedes, implementação do método para obter a quantidade de hóspedes e cálculo do valor da diária com desconto para reservas de 10 dias ou mais. Em Program.cs, foram atualizados os nomes dos hóspedes, alterado o tipo da suíte para "Premium" e ajustada a reserva para 11 dias, ativando a regra de desconto. Ajustes nas saídas do console para refletir as mudanças.

--- Program.cs
+++ Program.cs
@@ -2,24 +2,24 @@
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
-
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+ 
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Felipe", sobrenome: "Alves");
+Pessoa p2 = new Pessoa(nome: "Pedor", sobrenome: "Costa");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
 
-// Cria a suíte
+ 
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+
+Reserva reserva = new Reserva(diasReservados: 11);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
+ 
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+ 
